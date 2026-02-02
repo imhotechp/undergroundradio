7e5fe329cdd54c2d04ef4f43fc6819444ce4f0a0
IMHOTECHP <info@imhotechp.com>
2026-02-02 15:43:43 -0500

    fix pk directory

--- ug_radio_django/settings.py
+++ ug_radio_django/settings.py
@@ -16,7 +16,7 @@ from datetime import timedelta
 load_dotenv()  # loads .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-P_DIR = Path('/srv/id_rsa.pem')
+P_DIR = Path('/srv/id_rsa')
 PUB_DIR = Path('/srv/id_rsa_pub.pem')
 SIMPLE_JWT = {
     "ALGORITHM": "RS256",
