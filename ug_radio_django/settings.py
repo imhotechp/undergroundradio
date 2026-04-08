5e76bf33c0b37704e6ada868911ac9717b68d326
IMHOTECHP <info@imhotechp.com>
2026-04-08 17:41:56 -0400

    rekey the car

--- ug_radio_django/settings.py
+++ ug_radio_django/settings.py
@@ -16,8 +16,8 @@ from datetime import timedelta
 load_dotenv()  # loads .env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-P_DIR = Path('/srv/id_rsa.pem')
-PUB_DIR = Path('/srv/id_rsa_pub.pem')
+P_DIR = Path('/srv/mp3private.key')
+PUB_DIR = Path('/srv/mp3public.key')
 SIMPLE_JWT = {
     "ALGORITHM": "RS256",
     "SIGNING_KEY": P_DIR.read_text(),
