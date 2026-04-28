15576221aa3ed348afaf6ac43e481b3fc62c0e65
IMHOTECHP <info@imhotechp.com>
2026-04-27 20:10:06 -0400

    rename shit

--- ug_radio_ux/next.config.js
+++ ug_radio_ux/next.config.js
@@ -2,10 +2,9 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  reactStrictMode: true,
+  allowedDevOrigins:  ['72.61.75.183']
 };
 
-module.exports = {
-  allowedDevOrigins: ['72.61.75.183'],
-}
 
 export default nextConfig;
