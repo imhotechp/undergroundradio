0942eb5b724632d70353a1be4f9b3e5c663841bf
IMHOTECHP <info@imhotechp.com>
2026-05-01 21:10:31 -0400

    weg

--- ug_radio_ux/src/app/account/page.js
+++ ug_radio_ux/src/app/account/page.js
@@ -1,1 +1,11 @@
 // Component for musicv2/
+
+"use client"
+
+export default function login(){
+    return (
+        <>
+        <p>peon</p>
+        </>
+    )
+}
--- ug_radio_ux/src/app/add/page.js
+++ ug_radio_ux/src/app/add/page.js
@@ -1,2 +1,11 @@
 // Loading component showing status of songs being added after acc creation
 // add/
+"use client"
+
+export default function login(){
+    return (
+        <>
+        <p>peon</p>
+        </>
+    )
+}
