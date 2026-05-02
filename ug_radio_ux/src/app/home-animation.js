9aa6f261c5221222ead7e78df6c893439b419877
IMHOTECHP <info@imhotechp.com>
2026-05-01 21:04:40 -0400

    ss

--- ug_radio_ux/src/app/home-animation.js
+++ ug_radio_ux/src/app/home-animation.js
@@ -7,7 +7,7 @@ export default function BackgroundText(){
   const SVG = createSVG() 
   return (
   <>
-  <motion.svg  class="full-screen-svg" viewBox="-500 1000 3000 1200">
+  <motion.svg  className="full-screen-svg" viewBox="-500 1000 3000 1200">
     {SVG.map((d, i) => (
       <motion.path key={i} d={d} animate={{fill:'white'}}/>
     ))}
