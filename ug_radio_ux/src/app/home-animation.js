36960bfa38e921394c2f17f2280119c315a21cf0
IMHOTECHP <info@imhotechp.com>
2026-05-01 21:01:01 -0400

    lets see

--- ug_radio_ux/src/app/home-animation.js
+++ ug_radio_ux/src/app/home-animation.js
@@ -1,17 +1,15 @@
 "use client"
 import {createSVG} from "./svg.js";
-import {motion} from 'framer-motion';
+import {motion,} from 'framer-motion';
 
 
 export default function BackgroundText(){
   const SVG = createSVG() 
   return (
   <>
-  <motion.svg
-  viewBox="-500 1000 3000 1200"
-  animate={{ viewBox: "100 0 200 200" }}>
+  <motion.svg viewBox="-500 1000 3000 1200">
     {SVG.map((d, i) => (
-      <motion.path key={i} d={d} />
+      <motion.path key={i} d={d} animate={{opacity: 3}} />
     ))}
   </motion.svg>
   </>
