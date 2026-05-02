8dc6447dc9294ce2a458da0bc0b343d8d9ee5092
IMHOTECHP <info@imhotechp.com>
2026-05-01 20:23:03 -0400

    dd

--- ug_radio_ux/src/app/layout.js
+++ ug_radio_ux/src/app/layout.js
@@ -42,7 +42,7 @@ function BackgroundText(){
   const SVG = createSVG() 
   return (
   <>
-    <svg viewBox="0 0 3000 1000" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 3000 2000" xmlns="http://www.w3.org/2000/svg">
      {SVG.map((d, i) => (
       <path key={i} d={d} />
     ))}
